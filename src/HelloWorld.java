11215f7f9d446cd255d4d4d93482a7a3a488c06a
ZHOU William <12206665@crit-xubuntu>
2023-03-30 16:31:59 +0200

    modification

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
-<<<<<<< HEAD
-		System.out.println("Olá mundo !");
-=======
-		System.out.println("Hallo Welt !");
->>>>>>> a30c4ea4cc466da291261912507c36a323699f91
+		System.out.println("Bonjour le monde !");
 		System.out.println("Hola Mundo !");
 	}
 
